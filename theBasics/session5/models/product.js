e8062dde5f00b26d55635a32937751dcf6fb5a43
imdad Adelabou <imdadadelabou0@gmail.com>
2022-01-01 13:03:41 +0100

    refactor the code

--- theBasics/session5/models/product.js
+++ theBasics/session5/models/product.js
@@ -1,19 +1,25 @@
 const fs = require("fs");
 const path = require("path");
 
-
+function getProductFromFile(cb) {
+    let filePath = path.join(__dirname, '../', 'data', 'products.json');
+    fs.readFile(filePath, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        } else {
+            cb(JSON.parse(fileContent));
+        }
+    });
+};
 // const filePath = path.join(path.dirname(require.main.filename), 'data', 'products.json');
 class Product {
     constructor(title) {
         this.title = title;
     }
+
     save() {
         let filePath = path.join(__dirname, '../', 'data', 'products.json');
-        fs.readFile(filePath, (err, content) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(content);
-            }
+        getProductFromFile((products) => {
             products.push(this);
             fs.writeFile(filePath, JSON.stringify(products), (err) => {
                 console.log(err);
@@ -21,12 +27,8 @@ class Product {
         });
     }
     static fetchAll(cb) {
-        let filePath = path.join(__dirname, '../', 'data', 'products.json');
-        fs.readFile(filePath, (err, fileContent) => {
-            if (err) {
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
+        getProductFromFile((products) => {
+            cb(products);
         });
     }
 };
